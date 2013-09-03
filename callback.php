66f0ab0c8bbad30154429eae7f09c4b2f696e584
unknown <thachnuida@gmail.com>
2013-09-03 17:35:34 +0700

    Init

---
+++ callback.php
@@ -0,0 +1,41 @@
+<?php
+    session_start();
+    
+    $app_id = "";
+    $app_secret = "";
+    $redirect_uri = urlencode("http://localhost/fblogin/callback.php");    
+    
+    // Get code value
+    $code = $_GET['code'];
+    
+    // Get access token info
+    $facebook_access_token_uri = "https://graph.facebook.com/oauth/access_token?client_id=$app_id&redirect_uri=$redirect_uri&client_secret=$app_secret&code=$code";    
+    
+    $ch = curl_init(); 
+    curl_setopt($ch, CURLOPT_URL, $facebook_access_token_uri);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);    
+        
+    $response = curl_exec($ch); 
+    curl_close($ch);
+    
+    // Get access token
+    $access_token = str_replace('access_token=', '', explode("&", $response)[0]);
+    
+    // Get user infomation
+    $ch = curl_init(); 
+    curl_setopt($ch, CURLOPT_URL, "https://graph.facebook.com/me?access_token=$access_token");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);    
+        
+    $response = curl_exec($ch); 
+    curl_close($ch);
+    
+    $user = json_decode($response);   
+    
+    // Log user in
+    $_SESSION['user_login'] = true;
+    $_SESSION['user_name'] = $user->username;
+    
+    echo "Wellcome ". $user->username ."!";    
+?>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+ 
+<html>
+ 
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>Facebook login</title>
+    </head>
+ 
+    <body>
+ 
+        <a href="https://www.facebook.com/dialog/oauth?client_id=509011619175617&redirect_uri=http://localhost/fblogin/callback.php"><img src="https://ilovecuteshoes.com/skin/frontend/gravdept/acumen/img/facebook-login-button.png"/></a>
+ 
+    </body>
+ 
+</html>
